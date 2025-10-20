9c0d4a7c0dcc6135cc5323265fe2a7220566d578
Rabbul-Khan <rabbul.khan99@gmail.com>
2025-10-20 21:53:15 +0600

    Add eslint, antfu config for eslint and eslint-plugin-format
    Update eslint config file
    
    Update nuxt config file
    
    Update README.md
    
    Create .vscode settings.json file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// eslint-disable-next-line no-undef
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxt/eslint'],
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
 })
